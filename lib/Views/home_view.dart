40e4979de29774519d8bbd21369dc426c9526f08
jalalsowat <jalalalsowat@gmail.com>
2024-04-05 13:58:45 +0300

    create search view

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/Views/search_view.dart';
 import 'package:weather_app/Widgets/weather_info_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,6 +10,21 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Weather App"),
+        backgroundColor: Colors.grey,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const SearchView();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.search),
+          )
+        ],
       ),
       body: const WeatherInfoBody(),
     );
