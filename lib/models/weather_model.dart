0b6ac463936050c39cf5fb46a5859484d6f0163f
jalalsowat <jalalalsowat@gmail.com>
2024-04-06 18:22:08 +0300

    create weather service

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,18 +1,30 @@
 class WeatherModel {
   final String cityName;
   final String date;
-  final String? image;
-  final String temp;
-  final String maxTemp;
-  final String minTemp;
+  final String image;
+  final double temp;
+  final double maxTemp;
+  final double minTemp;
   final String weathercCondition;
 
   WeatherModel(
       {required this.cityName,
       required this.date,
-      this.image,
+      required this.image,
       required this.temp,
       required this.maxTemp,
       required this.minTemp,
       required this.weathercCondition});
+
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+        cityName: json["location"]["name"],
+        date: json["current"]["last_updated"],
+        image: json["forecast"]["forecastday"][0]["day"]["condition"]["icon"],
+        temp: json["forecast"]["forecastday"][0]["day"]["avgtemp_c"],
+        maxTemp: json["forecast"]["forecastday"][0]["day"]["maxtemp_c"],
+        minTemp: json["forecast"]["forecastday"][0]["day"]["mintemp_c"],
+        weathercCondition: json["forecast"]["forecastday"][0]["day"]
+            ["condition"]["text"]);
+  }
 }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,1 +1,17 @@
-class WeatherService {}
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/weather_model.dart';
+
+class WeatherService {
+  final Dio dio;
+  final String baseUrl = "https://api.weatherapi.com/v1";
+  final String apiKey = "d4872be5bf6f40c3891215247240504";
+
+  WeatherService(this.dio);
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    Response responce =
+        await dio.get("$baseUrl/forecast.json?key$apiKey=&q=$cityName&days=1");
+    WeatherModel weatherModel = WeatherModel.fromJson(responce.data);
+    return weatherModel;
+  }
+}
